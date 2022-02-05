526f51d270f5fa18e1d305c09dc793d322098ff3
SohAdrian <86836181+SohAdrian@users.noreply.github.com>
2022-02-05 12:23:21 +0800

    Re-amended GamePage.jsp into add New Game page

--- PopGamers/src/main/java/GameServlet.java
+++ PopGamers/src/main/java/GameServlet.java
@@ -2,18 +2,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+//Import these libraries from java.io and java.sql
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  * Servlet implementation class GameServlet
@@ -21,15 +21,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/GameServlet")
 public class GameServlet extends HttpServlet {
 
-	// Step 1: Prepare list of variables used for database connections
-	private String jdbcURL = "jdbc:mysql://localhost:3306/gamesdetails";
-	private String jdbcUsername = "root";
-	private String jdbcPassword = "";
-
-	// Step 2: Prepare list of SQL prepared statements to perform Retrieve to our
-	// database
-	private static final String SELECT_GAME_BY_ID = "select gameName, gamePicture, gameDescription, genre from gamedetails where gameName =?";
-	private static final String SELECT_ALL_GAMES = "select * from gamedetails";
 
 	private static final long serialVersionUID = 1L;
 
@@ -41,25 +32,6 @@ public class GameServlet extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
-	// Step 3: Implement the getConnection method which facilitates connection to
-	// the database via JDBC
-	protected Connection getConnection() {
-
-		Connection connection = null;
-
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		return connection;
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -68,78 +40,11 @@ public class GameServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// Step 4: Depending on the request servlet path, determine the function to
-		// invoke using the follow switch statement.
-
-		String action = request.getServletPath();
-		
-		try {
-			switch (action) {
-
-			case "/insert":		
-				break;
-				
-			case "/delete":
-				break;
-				
-			case "/edit":
-				break;
-				
-			case "/update":
-				break;
-
-			default:
-
-				listGames(request, response);
-				break;
-			}
-		}
-
-		catch (SQLException ex) {
-			throw new ServletException(ex);
-		}
 
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	// Step 5: listGames function to connect to the database and retrieve all games
-	// records
-	private void listGames(HttpServletRequest request, HttpServletResponse response)
-			throws SQLException, IOException, ServletException {
-
-		List<Game> games = new ArrayList<>();
-		try (Connection connection = getConnection();
-
-				// Step 5.1: Create a statement using connection object
-				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_GAMES);) {
-
-			// Step 5.2: Execute the query or update query
-			ResultSet rs = preparedStatement.executeQuery();
-
-			// Step 5.3: Process the ResultSet object.
-			while (rs.next()) {
-				String gameName = rs.getString("gameName");
-
-				// set session storage
-				HttpSession session = request.getSession();
-				session.setAttribute("gameName", gameName);
-
-				String gamePicture = rs.getString("gamePicture");
-				String gameDescription = rs.getString("gameDescription");
-				String genre = rs.getString("genre");
-				games.add(new Game(gameName, gamePicture, gameDescription, genre));
-			}
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-
-		// Step 5.4: Set the games list into the listGames attribute to be pass to the
-		// GameReview.jsp
-		request.setAttribute("listGames", games);
-		request.getRequestDispatcher("/GamePage.jsp").forward(request, response);
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
@@ -149,6 +54,55 @@ public class GameServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+
+		response.setContentType("text/html");
+		// Step 1: Initialize a PrintWriter object to return the html values via the
+		// response
+		
+		PrintWriter out = response.getWriter();
+		// Step 2: retrieve the four parameters from the request from the web form
+		String n = request.getParameter("gameName");
+		String p = request.getParameter("gamePicture");
+		String d = request.getParameter("gameDescription");
+		String g = request.getParameter("genre");
+
+		// Step 3: attempt connection to database using JDBC, you can change the
+		// username and passwordaccordingly using the phpMyAdmin > User Account
+		// dashboard
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamesdetails", "root", "");
+			// Step 4: implement the sql query using prepared statement
+			// (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
+
+			PreparedStatement ps = con.prepareStatement("insert into GAMEDETAILS values(?,?,?,?)");
+
+			// Step 5: parse in the data retrieved from the web form request into the
+			// prepared statement accordingly
+			ps.setString(1, n);
+			ps.setString(2, p);
+			ps.setString(3, d);
+			ps.setString(4, g);
+
+			// Step 6: perform the query on the database using the prepared statement
+			int i = ps.executeUpdate();
+
+			// Step 7: check if the query had been successfully execute, return “You are
+			// successfully registered” via the response,
+			if (i > 0) {
+				
+				PrintWriter writer = response.getWriter();
+				writer.println("<h1>" + "You have successfully Added a Game" + "</h1>");
+				writer.close();
+			}
+		}
+		// Step 8: catch and print out any exception
+		catch (Exception exception) {
+			System.out.println(exception);
+			out.close();
+		}
+		doGet(request, response);
+
 	}
 
 }
--- PopGamers/src/main/webapp/GamePage.jsp
+++ PopGamers/src/main/webapp/GamePage.jsp
@@ -9,36 +9,16 @@
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 	crossorigin="anonymous">
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>PopGamers</title>
 </head>
 <body>
-	<div>
-		<button class="button"
-			href="<%=request.getContextPath()%>/DiscussionPage.jsp">Game
-			Forum</button>
-	</div>
-	<h1>PopGamers Home Page</h1>
-	<div class="row center" style="float: none; margin: 0 auto;">
-		<div class="card">
-			<c:forEach var="game" items="${listGames}">
-				<!-- <a href="#"> -->
-				<img class="img-fluid img-thumbnail" src="${game.gamePicture}"
-					width="400px" height="200px" />
-				<br>
-				<p>
-					Game Name: <b>${game.gameName}</b>
-				</p>
-				<p>Description: ${game.gameDescription}</p>
-				 <p>Genre: ${game.genre}</p>
-				<!-- </a> -->
-				<div>
-					<button class="button"
-						href="<%=request.getContextPath()%>/GameReview.jsp">See
-						Review</button>
-					<br>
-				</div>
-			</c:forEach>
-		</div>
-	</div>
+<h1>Add a New Game</h1>
+	<form action="GameServlet" method="post">
+		<br> Game Name: <input type="text" name="gameName"><br>
+		gamePicture: <input type="text" name="gamePicture"><br>
+		gameDescription: <input type="text" name="gameDescription"><br>
+		Game's Genre: <input type="text" name="genre"><br> <input
+			type="submit" value="Add Game?" />
+	</form>
 </body>
 </html>
