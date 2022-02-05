f4d3520e3c10fc91897104ac8c0aed7d88e40957
MuhdThoha <thoha_13@hotmail.sg>
2022-02-05 20:05:27 +0800

    Created User dashboard without CRUD functions

--- src/main/java/GameUserServlet.java
+++ src/main/java/GameUserServlet.java
@@ -27,6 +27,59 @@ public class GameUserServlet extends HttpServlet {
 	public GameUserServlet() {
 		super();
 		// TODO Auto-generated constructor stub
+		// Step 1: Prepare list of variables used for database connections
+	}
+
+	private String jdbcURL = "jdbc:mysql://localhost:3306/gameuserdetails";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "";
+	// Step 2: Prepare list of SQL prepared statements to perform CRUD to our
+	// database
+	private static final String INSERT_USERS_SQL = "INSERT INTO GameUserDetails" + " (username, password, email) VALUES "
+			+ " (?, ?);";
+	private static final String SELECT_USER_BY_ID = "select username,password,email,language from GameUserDetails where username =?";
+	private static final String SELECT_ALL_USERS = "select * from GameUserDetails ";
+	private static final String DELETE_USERS_SQL = "delete from GameUserDetails where username = ?;";
+	private static final String UPDATE_USERS_SQL = "update GameUserDetails set name = ?,password= ?, email =? where username = ?;";
+
+	// Step 3: Implement the getConnection method which facilitates connection to
+	// the database via JDBC
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	// Step 5: listUsers function to connect to the database and retrieve all users
+	// records
+	private void listUsers(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		List<GameUser> users = new ArrayList<>();
+		try (Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
+			// Step 5.2: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+			// Step 5.3: Process the ResultSet object.
+			while (rs.next()) {
+				String name = rs.getString("username");
+				String password = rs.getString("password");
+				String email = rs.getString("email");
+				users.add(new GameUser(name, password, email));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		// Step 5.4: Set the users list into the listUsers attribute to be pass to the
+		// userManagement.jsp
+		request.setAttribute("listUsers", users);
+		request.getRequestDispatcher("/gameUserManagement.jsp").forward(request, response);
 	}
 
 	/**
@@ -37,6 +90,26 @@ public class GameUserServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Step 4: Depending on the request servlet path, determine the function to
+		// invoke using the follow switch statement.
+		String action = request.getServletPath();
+		try {
+			switch (action) {
+			case "/insert":
+				break;
+			case "/delete":
+				break;
+			case "/edit":
+				break;
+			case "/update":
+				break;
+			default:
+				listUsers(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
 	}
 
 	/**
