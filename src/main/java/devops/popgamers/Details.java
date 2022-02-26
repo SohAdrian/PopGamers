6d25750abf3963e71179f57c53865186e1e014c8
SohAdrian <86836181+SohAdrian@users.noreply.github.com>
2022-02-26 19:54:26 +0800

    Files that are amended by Adrian

---
+++ src/main/java/devops/popgamers/Details.java
@@ -0,0 +1,26 @@
+package devops.popgamers;
+
+public class Details {
+	protected String discussion;
+	protected String time;
+	
+	public Details(String discussion, String time) {
+		super();
+		this.discussion = discussion;
+		this.time = time;
+	}
+	public String getDiscussion() {
+		return discussion;
+	}
+	
+	public void setDiscussion(String discussion) {
+		this.discussion = discussion;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void	setTime(String time) {
+		this.time = time;
+	}
+
+}
